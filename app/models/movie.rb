639888912d57fb2e999f53299faf9f191bf093fc
alban bengounia <alban.bengounia@gmail.com>
2024-12-16 15:09:22 +0100

    translated genres + icons

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,28 +1,29 @@
 class Movie < ApplicationRecord
-  GENRES = [
-    'Action',
-    'Adventure',
-    'Animation',
-    'Comedy',
-    'Crime',
-    'Documentary',
-    'Drama',
-    'Family',
-    'Fantasy',
-    'History',
-    'Horror',
-    'Music',
-    'Mystery',
-    'News',
-    'Non spécifié',
-    'Reality',
-    'Romance',
-    'Science fiction',
-    'Talk show',
-    'Thriller',
-    'War',
-    'Western'
-  ]
+  GENRES = {
+    'Action' => { name: 'Action', icon: 'fas fa-fist-raised' },
+    'Adventure' => { name: 'Aventure', icon: 'fas fa-hiking' },
+    'Animation' => { name: 'Animation', icon: 'fas fa-film' },
+    'Comedy' => { name: 'Comédie', icon: 'fas fa-laugh' },
+    'Crime' => { name: 'Crime', icon: 'fas fa-user-secret' },
+    'Documentary' => { name: 'Documentaire', icon: 'fas fa-book-open' },
+    'Drama' => { name: 'Drame', icon: 'fas fa-theater-masks' },
+    'Family' => { name: 'Famille', icon: 'fas fa-home' },
+    'Fantasy' => { name: 'Fantastique', icon: 'fas fa-hat-wizard' },
+    'History' => { name: 'Histoire', icon: 'fas fa-landmark' },
+    'Horror' => { name: 'Horreur', icon: 'fas fa-ghost' },
+    'Music' => { name: 'Musique', icon: 'fas fa-music' },
+    'Mystery' => { name: 'Mystère', icon: 'fas fa-magnifying-glass' },
+    'News' => { name: 'Actualités', icon: 'fas fa-newspaper' },
+    'Non spécifié' => { name: 'Non spécifié', icon: 'fas fa-question' },
+    'Reality' => { name: 'Télé-réalité', icon: 'fas fa-tv' },
+    'Romance' => { name: 'Romance', icon: 'fas fa-heart' },
+    'Science fiction' => { name: 'Science-fiction', icon: 'fas fa-robot' },
+    'Talk show' => { name: 'Talk-show', icon: 'fas fa-comments' },
+    'Thriller' => { name: 'Thriller', icon: 'fas fa-skull-crossbones' },
+    'War' => { name: 'Guerre', icon: 'fas fa-person-rifle' },
+    'Western' => { name: 'Western', icon: 'fas fa-hat-cowboy' }
+  }
+
 
   has_many :service_shows
   has_many :services, through: :service_shows
@@ -44,6 +45,11 @@ class Movie < ApplicationRecord
 
   # scope pour rechercher les films par genre @noah ?
   #scope :by_genre, ->(genre) { where("genres LIKE ?", "%#{genre}%") }
+
+  def translated_genres
+    genres_list = genres.split(',').map(&:strip)
+    genres_list.map { |genre| GENRES[genre] || genre }.join(', ')
+  end
 
   private
 
